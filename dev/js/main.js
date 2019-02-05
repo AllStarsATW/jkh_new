5272758c2faf76e12cef03113c7a8f81b1f08737
atw <allstarsatw@gmail.com>
2019-02-06 01:59:23 +0300

    - Добавлено и стилизовано мобильное меню Выполнил через aside, т.е. на сайте есть дубль меню для мобильной версии

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,31 +1,49 @@
 $(document).ready(function() {
-    $("#topline-search__icon").click(function () {
-            $(this).closest("#topline-search").addClass("i-active").find("input").focus()
+    var $header = $("#header-wrap");
+    $(window).scroll(function(){
+        if ( $(this).scrollTop() > 100 && $header.hasClass("default") ){
+            $header.fadeOut('fast',function(){
+                $(this).removeClass("default")
+                    .addClass("fixed")
+                    .slideDown('fast');
+            });
+        } else if($(this).scrollTop() <= 100 && $header.hasClass("fixed")) {
+            $header.fadeOut('fast',function(){
+                $(this).removeClass("fixed")
+                    .addClass("default")
+                    .fadeIn('fast');
+            });
+        }
+    });//scroll
+    $("#menu-toggle").click(function () {
+            $(this).toggleClass('mobile-exit');
+            $("#mobile-sidebar").toggleClass("i-active");
+            $("#main-overlay").toggleClass("i-active");
+            event.stopPropagation();
         }
     );
-    $(function () {
-        $(document).click(function (event) {
-            if ($(event.target).closest("#topline-search").length) return;
-            $("#topline-search").removeClass("i-active");
+    $("#main-overlay").click(function () {
+            $("#menu-toggle").removeClass("mobile-exit");
+            $("#mobile-sidebar").removeClass("i-active");
+            $("#main-overlay").removeClass("i-active");
             event.stopPropagation();
-        });
+        }
+    );
+    $('.menu-item_lvl-1').click(function(event) {
+        $('._opened').removeClass('_opened');
+        $(this).addClass('_opened');
     });
-    $(".header-menu__menu-toggle").click(function () {
-            $(".header-menu__list").addClass("header-menu__list_active");
-            $(".header-menu__overlay").addClass("header-menu__overlay_active");
-        }
-    );
-    $(".header-menu__mobile-exit").click(function () {
-            $(".header-menu__list").removeClass("header-menu__list_active");
-            $(".header-menu__overlay").removeClass("header-menu__overlay_active");
-            event.stopPropagation();
-        }
-    );
-    $(".header-menu__overlay").click(function () {
-            $(".header-menu__list").removeClass("header-menu__list_active");
-            $(".header-menu__overlay").removeClass("header-menu__overlay_active");
-            event.stopPropagation();
-        }
-    );
+    $('.back-link_lvl-1').click(function(event) {
+        $('.menu-item_lvl-1').removeClass('_opened');
+        return false;
+    });
+    $('.menu-item_lvl-2').click(function(event) {
+        $(this).addClass('_opened');
+        return false;
+    });
+    $('.back-link_lvl-2').click(function(event) {
+        $('.menu-item_lvl-2').removeClass('_opened');
+        return false;
+    });
 
 });
